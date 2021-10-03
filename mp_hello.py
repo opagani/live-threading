8b470fdb46e3861eeea48a075e0b1c53f237a8fd
Reuven Lerner <reuven@lerner.co.il>
2021-10-03 21:22:11 +0300

    gitautosync update 80

---
+++ mp_hello.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import multiprocessing
+import time
+import random
+
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    print(f'{n} Hello!\n', end='')
+
+
+all_threads = []
+for i in range(10):
+    t = threading.Thread(target=hello, args=(i,), name=f'hello-{i}')
+    t.start()
+    all_threads.append(t)
+
+while all_threads:
+    for one_thread in all_threads:
+        start_wait = time.time()
+        print(f'Now waiting on thread {one_thread.name}')
+        one_thread.join(0.001)
+        if not one_thread.is_alive():
+            print(
+                f'\tTerminated thread {one_thread.name} after {time.time() - start_wait} secs')
+            all_threads.remove(one_thread)
+
+print('Done!')
