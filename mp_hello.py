5c68f94a03e80fc45cb1960c9b24a60b20520365
Reuven Lerner <reuven@lerner.co.il>
2021-10-03 21:25:38 +0300

    gitautosync update 88

--- mp_hello.py
+++ mp_hello.py
@@ -10,20 +10,22 @@ def hello(n):
     print(f'{n} Hello!\n', end='')
 
 
-all_processes = []
-for i in range(10):
-    p = multiprocessing.Process(target=hello, args=(i,), name=f'hello-{i}')
-    p.start()
-    all_processes.append(p)
+if __name__ == '__main__':
 
-while all_processes:
-    for one_process in all_processes:
-        start_wait = time.time()
-        print(f'Now waiting on process {one_process.name}')
-        one_process.join(0.001)
-        if not one_process.is_alive():
-            print(
-                f'\tTerminated process {one_process.name} after {time.time() - start_wait} secs')
-            all_processes.remove(one_process)
+    all_processes = []
+    for i in range(10):
+        p = multiprocessing.Process(target=hello, args=(i,), name=f'hello-{i}')
+        p.start()
+        all_processes.append(p)
 
-print('Done!')
+    while all_processes:
+        for one_process in all_processes:
+            start_wait = time.time()
+            print(f'Now waiting on process {one_process.name}')
+            one_process.join(0.001)
+            if not one_process.is_alive():
+                print(
+                    f'\tTerminated process {one_process.name} after {time.time() - start_wait} secs')
+                all_processes.remove(one_process)
+
+    print('Done!')
